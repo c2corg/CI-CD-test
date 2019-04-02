bf22ebf89e0c71118f89f85d79ba2d2ac2a6325a
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2019-04-02 13:01:48 +0200

    clean script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,12 @@
-# clone 
+# clone repository on gh-pages branch. Save it in gh-pages folder 
 git clone --single-branch --branch=gh-pages https://${GITHUB_TOKEN}@github.com/c2corg/CI-CD-test.git gh-pages
+# git clone --single-branch --branch=gh-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
 
-# move new folder
+# move build into gh-pages folder, under a new folder with <branch-name> as name
 mv dist gh-pages/$TRAVIS_BRANCH
 
+# Add this folder into gh-pages branch, commit and push
 cd gh-pages
-ls -la
-
-git branch
-
 git add .
 git commit -m "Deploy $TRAVIS_BRANCH branch"
-
 git push
