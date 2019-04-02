b5a0335a7422712a68ea3b8e6ca8651e07450bd8
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2019-04-02 13:15:36 +0200

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,7 @@ echo "TRAVIS_PULL_REQUEST = $TRAVIS_PULL_REQUEST"
 git clone --single-branch --branch=gh-pages https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
 
 # move build into gh-pages folder, under a new folder with <branch-name> as name
+rm -rf gh-pages/$TRAVIS_BRANCH
 mv dist gh-pages/$TRAVIS_BRANCH
 
 # Add this folder into gh-pages branch, commit and push
