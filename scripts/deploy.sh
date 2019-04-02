0d3766ed0981d4d7fb646053b548e10e2bdbcf28
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2019-04-02 12:54:30 +0200

    token

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 # clone 
-git clone --single-branch --branch=gh-pages https://github.com/c2corg/CI-CD-test.git gh-pages
+git clone --single-branch --branch=gh-pages https://${GITHUB_TOKEN}@github.com/c2corg/CI-CD-test.git gh-pages
 
 # move new folder
 mv dist gh-pages/$TRAVIS_BRANCH
@@ -9,7 +9,7 @@ ls -la
 
 git branch
 
-git add -A
+git add .
 git commit -m "Deploy $TRAVIS_BRANCH branch"
 
 git push
