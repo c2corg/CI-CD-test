066a94d6445d71d466abb7f6ed01563a019506de
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2019-04-26 15:34:31 +0200

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,7 +6,5 @@ if [ $TRAVIS_TAG ]; then
     firstTag=$(git tag | sort -r | head -1)
     secondTag=$(git tag | sort -r | head -2 | awk '{split($0, tags, "\n")} END {print tags[1]}')
     
-    echo "Changes between ${secondTag} and ${firstTag}\n"
-    
     git log  --pretty=oneline ${secondTag}..${firstTag} --no-merges | node scripts/update-release-description.js
 fi
