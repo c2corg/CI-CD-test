a134f2169113d232fb5d81d35039ac86dbb89978
Charles de Beauchesne <charles.de.beauchesne@gmail.com>
2019-04-27 11:27:31 +0200

    Update and rename release.sh to update-release-description.sh

--- scripts/update-release-description.sh
+++ scripts/update-release-description.sh
@@ -1,10 +1,7 @@
 
 # if it's a tag, add commit logs
 if [ $TRAVIS_TAG ]; then
-    echo "Add commit logs to tag description"
-    
     firstTag=$(git tag | sort -r | head -1)
     secondTag=$(git tag | sort -r | head -2 | awk '{split($0, tags, "\n")} END {print tags[1]}')
-    
     git log  --pretty=oneline ${secondTag}..${firstTag} --no-merges | node scripts/update-release-description.js
 fi
